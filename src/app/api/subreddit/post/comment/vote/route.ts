3e38fcbe293bcdc6fe2637130229745a559e01f6
tahaamin2013 <tahaamindob2013@gmail.com>
2023-12-24 20:47:22 +0500

    Fixed my error

--- src/app/api/subreddit/post/comment/vote/route.ts
+++ src/app/api/subreddit/post/comment/vote/route.ts
@@ -15,7 +15,7 @@ export async function PATCH(req: Request) {
       return new Response('Unauthorized', { status: 401 })
     }
 
-    // check if user has already voted on this post
+    // Check if user has already voted on this comment
     const existingVote = await db.commentVote.findFirst({
       where: {
         userId: session.user.id,
@@ -24,26 +24,27 @@ export async function PATCH(req: Request) {
     })
 
     if (existingVote) {
-      // if vote type is the same as existing vote, delete the vote
+      // If vote type is the same as the existing vote, delete the vote
       if (existingVote.type === voteType) {
         await db.commentVote.delete({
           where: {
-            userId_commentId: { 
-              commentId,
+            userId_commentId: {
               userId: session.user.id,
+              commentId,
             },
           },
+          
         })
         return new Response('OK')
       } else {
-        // if vote type is different, update the vote
+        // If vote type is different, update the vote
         await db.commentVote.update({
           where: {
             userId_commentId: {
-              commentId,
               userId: session.user.id,
+              commentId,
             },
-          },
+          },          
           data: {
             type: voteType,
           },
@@ -52,7 +53,7 @@ export async function PATCH(req: Request) {
       }
     }
 
-    // if no existing vote, create a new vote
+    // If no existing vote, create a new vote
     await db.commentVote.create({
       data: {
         type: voteType,
